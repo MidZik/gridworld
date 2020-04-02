357e881ab4f5ae3c0b569e53dc07f6990cc9d106
MidZik <matt.idzik1@gmail.com>
2020-04-02 17:30:29 -0500

    Removed entt header dependency from Registry.h

--- GridWorld/Registry.h
+++ GridWorld/Registry.h
@@ -1,11 +1,20 @@
 #pragma once
 
 #include <stdint.h>
-#include <entt/entt.hpp>
+
+namespace entt
+{
+    template<class>
+    class basic_registry;
+}
 
 namespace GridWorld
 {
-    ENTT_OPAQUE_TYPE(EntityId, uint64_t);
+    enum class EntityId : uint64_t { };
+    constexpr auto to_integral(const EntityId eid) noexcept
+    {
+        return static_cast<std::underlying_type_t<EntityId>>(eid);
+    }
 
     using registry = entt::basic_registry<EntityId>;
 }
